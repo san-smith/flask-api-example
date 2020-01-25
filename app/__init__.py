dedf3f813addad0d8ee2c78b2c893c2fd80ae516
san-smith <san-smith@mail.ru>
2020-01-25 21:08:27 +0700

    flask db init

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app: Flask = Flask(__name__, instance_relative_config=True)
 
@@ -11,5 +13,8 @@ app.config.from_pyfile('config.py')
 # Load the file specified by the APP_CONFIG_FILE environment variable
 # Variables defined here will override those in the default configuration
 # app.config.from_envvar('APP_CONFIG_FILE')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
