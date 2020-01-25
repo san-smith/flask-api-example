0bd201d516ce87be4cbe09ce62d82b4c55664f37
san-smith <san-smith@mail.ru>
2020-01-25 22:26:18 +0700

    add flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app: Flask = Flask(__name__, instance_relative_config=True)
 
@@ -16,6 +17,7 @@ app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
-from app import routes
 from app.models import user
+from app.api import routes
