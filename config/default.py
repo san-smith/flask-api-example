b0ccb9433234575ff8252767274e3bc00f872743
san-smith <san-smith@mail.ru>
2020-01-28 23:14:44 +0700

    Fix default config

--- config/default.py
+++ config/default.py
@@ -1,4 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = False
 SQLALCHEMY_ECHO = False
 JWT_TOKEN_PREFIX = 'Token'
 SECRET_KEY = 'secret'
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
